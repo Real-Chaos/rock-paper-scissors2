4b7d568ead44b00d0a8e15e15e782b09c4169e89
Chaos <rizwannafees13@gmail.com>
2021-03-10 17:35:13 -0500

    almost done

--- script.js
+++ script.js
@@ -5,12 +5,34 @@ nickname.forEach(nick => {
     nick.textContent = askNickname.slice(0, 10);
 });
 
+// Computer Move 
+
+
+const playerRock = document.querySelector('.playerRock');
+const playerPaper = document.querySelector('.playerPaper');
+const playerScissors = document.querySelector('.playerScissors');
+let playersMove = null;
+// // function playerMove() {
+// //     playerRock.addEventListener('click', ()=> {
+// //         playersMove = playerRock;
+// //         computerMove();
+// //     })
+// //     playerPaper.addEventListener('click', ()=> {
+// //         computerMove();
+// //     })
+// //     playerScissors.addEventListener('click', ()=> {
+// //         computerMove();
+// //     })
+// // }
+// playerMove();
+
 // Computer Move
+const rock = document.querySelector('.rock');
+const paper = document.querySelector('.paper');
+const scissors = document.querySelector('.scissors')
 
 function computerMove() {
-   const rock = document.querySelector('.rock');
-    const paper = document.querySelector('.paper');
-    const scissors = document.querySelector('.scissors')
+   
     let computerSelection = Math.floor((Math.random() * 3) + 1);
     switch(computerSelection) {
         case 1:
@@ -26,24 +48,114 @@ function computerMove() {
     return computerSelection;
 }
 
-// Player Move 
+// console.log(computerMove());
+// One Round
+let botScore = document.querySelector('.botScoreCount');
+let botScoreCount = Number(botScore.textContent);
 
-function playerMove() {
-    const playerRock = document.querySelector('.playerRock');
-    const playerPaper = document.querySelector('.playerPaper');
-    const playerScissors = document.querySelector('.playerScissors');
-    let playerSelection;
+
+let playerScore = document.querySelector('.playerScoreCount');
+let playerScoreCount = Number(playerScore.textContent);
+
+function game() {
     playerRock.addEventListener('click', () => {
-        playerSelection = playerRock;
-        return console.log(playerSelection);
+        if(computerMove() === paper) {
+            botScoreCount++;
+            botScore.textContent = botScoreCount;
+            console.log(computerMove());
+        }
+        else if(computerMove() === scissors) {
+            playerScoreCount++;
+            playerScore.textContent = playerScoreCount;
+            console.log(computerMove());
+        }
+        else if(botScoreCount === 5|| playerScoreCount === 5) {
+            botScoreCount = 0;
+            botScore.textContent = botScoreCount;
+            playerScoreCount = 0;
+            playerScore.textContent = botScoreCount;
+            console.log(computerMove());
+        }
     });
-
     playerPaper.addEventListener('click', () => {
-        playerSelection = playerPaper;
+        if(computerMove() === scissors) {
+            botScoreCount++;
+            botScore.textContent = botScoreCount;
+            console.log(computerMove());
+        }
+        else if(computerMove() === rock) {
+            playerScoreCount++;
+            playerScore.textContent = playerScoreCount;
+            console.log(computerMove());
+        }
+        else if(botScoreCount === 5|| playerScoreCount === 5) {
+            botScoreCount = 0;
+            botScore.textContent = botScoreCount;
+            playerScoreCount = 0;
+            playerScore.textContent = botScoreCount;
+            console.log(computerMove());
+        }
     });
     playerScissors.addEventListener('click', () => {
-        playerSelection = playerScissors;
-        });
-    }
-yes = playerMove();
-console.log(yes);
+        if(computerMove() === rock) {
+            botScoreCount++;
+            botScore.textContent = botScoreCount;
+            console.log(computerMove());
+        }
+        else if(computerMove() === paper) {
+            playerScoreCount++;
+            playerScore.textContent = playerScoreCount;
+            console.log(computerMove());
+        
+        }
+        else if(botScoreCount === 5|| playerScoreCount === 5) {
+            botScoreCount = 0;
+            botScore.textContent = botScoreCount;
+            playerScoreCount = 0;
+            playerScore.textContent = botScoreCount;
+            console.log(computerMove());
+        }
+    });
+
+
+}
+
+game();
+
+
+// // Game 
+// let botScore = document.querySelector('.botScoreCount');
+// let botScoreCount = Number(botScore.textContent);
+// console.log(typeof botScoreCount);
+
+// let playerScore = document.querySelector('.playerScoreCount');
+// let playerScoreCount = Number(botScore.textContent);
+// console.log(typeof playerScoreCount);
+
+// function playRound() {
+//     playerRock.addEventListener('click', () => {
+//         if(computerChoice === document.querySelector('.paper')) {
+//             botScoreCount++;
+//             botScore.textContent = botScoreCount;
+//             computerMove();
+//             console.log(botScore);       
+//         }
+//     });
+//     playerPaper.addEventListener('click', ()=> {
+//         if(computerChoice === document.querySelector('.scissors')) {
+//             botScoreCount++;
+//             botScore.textContent = botScoreCount;
+//             computerMove();
+        
+//         }
+//     });
+//     playerScissors.addEventListener('click', () => {
+//         if(computerChoice === document.querySelector('.rock')) {
+//             botScoreCount++
+//             botScore.textContent = botScoreCount;
+//             computerMove();
+//         }
+//     })
+// }
+// playRound();
+
