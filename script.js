04c96e0c2bee2c3da98c09764aa7b717b8b55dc9
Chaos <rizwannafees13@gmail.com>
2021-03-11 12:30:11 -0500

    Not done yet

--- script.js
+++ script.js
@@ -1,113 +1,107 @@
-// Create Nickname
-const askNickname = String(prompt('Enter players nickname'));
-const nickname = document.querySelectorAll('.nickname');
-nickname.forEach(nick => {
-    nick.textContent = askNickname.slice(0, 10);
-});
-
-// Computer Move 
-
-
+// Global Scope -------------------------------------------------------------------------------------------------------------------------------
 const playerRock = document.querySelector('.playerRock');
 const playerPaper = document.querySelector('.playerPaper');
-const playerScissors = document.querySelector('.playerScissors');
-let playersMove = null;
+const playerScissors = document.querySelector('.playerScissors')
 
-
-// Computer Move
-const rock = document.querySelector('.rock');
-const paper = document.querySelector('.paper');
-const scissors = document.querySelector('.scissors')
-
-function computerMove() {
-   
-    let computerSelection = Math.floor((Math.random() * 3) + 1);
-    switch(computerSelection) {
-        case 1:
-            computerSelection = rock;
-            break;
-        case 2:
-            computerSelection = paper;
-            break;
-        case 3:
-            computerSelection = scissors;
-            break;
-    }
-    return computerSelection;
-}
-
-
+let playerSelection = null;
 let botScore = document.querySelector('.botScoreCount');
 let botScoreCount = Number(botScore.textContent);
 
-
 let playerScore = document.querySelector('.playerScoreCount');
 let playerScoreCount = Number(playerScore.textContent);
+let result = document.querySelector('.result')
 
-function game() {
-    playerRock.addEventListener('click', () => {
-        if(computerMove() === paper) {
-            botScoreCount++;
-            botScore.textContent = botScoreCount;
-            console.log(computerMove());
-        }
-        else if(computerMove() === scissors) {
-            playerScoreCount++;
-            playerScore.textContent = playerScoreCount;
-            console.log(computerMove());
-        }
-        else if(botScoreCount >= 4|| playerScoreCount >= 4) {
-            botScoreCount = 0;
-            botScore.textContent = botScoreCount;
-            playerScoreCount = 0;
-            playerScore.textContent = botScoreCount;
-            console.log(computerMove());
-        }
-    });
-    playerPaper.addEventListener('click', () => {
-        if(computerMove() === scissors) {
-            botScoreCount++;
-            botScore.textContent = botScoreCount;
-            console.log(computerMove());
-        }
-        else if(computerMove() === rock) {
-            playerScoreCount++;
-            playerScore.textContent = playerScoreCount;
-            console.log(computerMove());
-        }
-        else if(botScoreCount >= 4|| playerScoreCount >= 4) {
-            botScoreCount = 0;
-            botScore.textContent = botScoreCount;
-            playerScoreCount = 0;
-            playerScore.textContent = botScoreCount;
-            console.log(computerMove());
-        }
-    });
-    playerScissors.addEventListener('click', () => {
-        if(computerMove() === rock) {
-            botScoreCount++;
-            botScore.textContent = botScoreCount;
-            console.log(computerMove());
-        }
-        else if(computerMove() === paper) {
-            playerScoreCount++;
-            playerScore.textContent = playerScoreCount;
-            console.log(computerMove());
-        
-        }
-        else if(botScoreCount >=4|| playerScoreCount >= 4) {
-            botScoreCount = 0;
-            botScore.textContent = botScoreCount;
-            playerScoreCount = 0;
-            playerScore.textContent = botScoreCount;
-            console.log(computerMove());
-        }
-    });
+// Create Nickname ----------------------------------------------------------------------------------------------------------------------------
+const askNickname = String(prompt('Enter players nickname'));
+const nickname = document.querySelector('.nickname');
+nickname.textContent = askNickname;
 
-
+// Computer Move --------------------------------------------------------------------------------------------------------------------------------
+function computerMove() {
+    let computerMove = Math.floor((Math.random() * 3) + 1);
+    switch(computerMove) {
+        case 1:
+            computerMove = 'rock';
+            break;
+        case 2:
+            computerMove = 'paper';
+            break;
+        case 3:
+            computerMove = 'scissors';
+            break;
+    }
+    return computerMove;
 }
 
-game();
+
+//listeners ---------------------------------------------------------------------------------------------------------------------------------------
+playerRock.addEventListener('click', () => {
+    playerSelection = 'rock';
+    let computerSelection = computerMove();
+    if(computerSelection === 'paper') {
+        botScoreCount++;
+        botScore.textContent = botScoreCount;
+        result.textContent = `Computer chose ${computerSelection}! You lose.`;
+    }
+
+    else if(computerSelection === 'scissors') {
+        playerScoreCount++;
+        playerScore.textContent = playerScoreCount;
+        result.textContent = `Computer chose ${computerSelection}! You win.`;
+
+    }
+
+    else if(computerSelection === 'rock') {
+        result.textContent = `Computer chose ${computerSelection}! Tie.`;
+    }
+        
+});
+
+playerPaper.addEventListener('click', ()=> {
+    playerSelection = 'paper';
+    let computerSelection = computerMove();
+    if(computerSelection === 'scissors') {
+        botScoreCount++;
+        botScore.textContent = botScoreCount;
+        result.textContent = `Computer chose ${computerSelection}! You lose.`;
+
+    }
+
+    else if(computerSelection === 'rock') {
+        playerScoreCount++;
+        playerScore.textContent = playerScoreCount;
+        result.textContent = `Computer chose ${computerSelection}! You win.`;
+    }
+
+    else if(computerSelection === 'paper') {
+        result.textContent = `Computer chose ${computerSelection}! Tie.`;
+    }
+        
+});
+
+playerScissors.addEventListener('click', ()=> {
+    playerSelection = 'scissors';
+    let computerSelection = computerMove();
+    if(computerSelection === 'rock') {
+        botScoreCount++;
+        botScore.textContent = botScoreCount;
+        result.textContent = `Computer chose ${computerSelection}! You lose.`;
+    }
+
+    else if(computerSelection === 'paper') {
+        playerScoreCount++;
+        playerScore.textContent = playerScoreCount;
+        result.textContent = `Computer chose ${computerSelection}! You win.`;
+    }
+
+    else if(computerSelection === 'scissors') {
+        result.textContent = `Computer chose ${computerSelection}! Tie.`;
+    }
+
+        
+});
+
+
 
 
 
